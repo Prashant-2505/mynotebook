54ed22c522f3569a1841197f10367579b809eb68
Prashant-2505 <sharmaprashant2505@gmail.com>
2022-12-15 16:53:56 +0530

    commenting

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,6 +5,7 @@ const { body, validationResult } = require('express-validator');
 
 
 // create a user using: POST "/api/auth"
+// using express validator to validate the elemnts
 router.post('/createuser', [
     body('email', 'enter the valid mail').isEmail(),
     body('name', 'enter the valid name').isLength({ min: 3 }),
@@ -17,13 +18,22 @@ router.post('/createuser', [
         return res.status(400).json({ errors: errors.array() });
     }
     // check weather the user with this
-    let user = await User.create
+    // findone is mongoose method ===> used to find the element prsnt or not
+    let user = await User.findOne({email: req.body.email})
+    if(user)
+    {
+        return res.status(400).json({error:"sorry user with that email is already exist"})
+    }
+
+    // create is moongoose method 
+    // here in User model where scehma is presnet create will create user and take details from body and send to User model/in Database
+     user = await User.create
     ({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password,
     })
-    .then(user => res.json(user));
+    .then(user => res.json(user));   // here we send the the response as json usin arrow function inside .then  to tha console
    
 })
 
